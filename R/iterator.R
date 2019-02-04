d5314f835b5a6279d8331f9c61f20b216e73f1a3
Drew Tyre <atyre2@ip-172-31-30-120.us-west-2.compute.internal>
2019-02-04 15:32:48 -0600

    not working

--- R/iterator.R
+++ R/iterator.R
@@ -1,8 +1,8 @@
-iterate_pop <- function(popfun = NULL, parms = list()){
+iterate_pop <- function(popfun = NULL, parms = NULL){
   if (is.null(popfun)) stop("must provide population function")
-  if (length(parms) == 0) stop("must provide parameter list")
-  N <- vector("numeric", length = length(parms$t))
-  N[1] <- parms$N0  # first entry in vector is initial population size
+  if (is.null(parms)) stop("must provide parameter dataframe")
+  N <- vector("numeric", length = parms$t)
+  N[1] <- parms$N0[1]  # first entry in vector is initial population size
   last_t <- length(N)
   # these next lines will cause warnings if
   # b is not either length 1 or (t+1)
