4d78a46bfccfde26afe6dc56d61647961dc3cd9a
Drew Tyre <atyre2@ip-172-31-30-120.us-west-2.compute.internal>
2019-02-04 15:27:47 -0600

    First cut at iterator

---
+++ R/iterator.R
@@ -0,0 +1,23 @@
+iterate_pop <- function(popfun = NULL, parms = list()){
+  if (is.null(popfun)) stop("must provide population function")
+  if (length(parms) == 0) stop("must provide parameter list")
+  N <- vector("numeric", length = length(parms$t))
+  N[1] <- parms$N0  # first entry in vector is initial population size
+  last_t <- length(N)
+  # these next lines will cause warnings if
+  # b is not either length 1 or (t+1)
+  if (length(parms$b) != last_t) parms$b <- rep(parms$b, times=(last_t))
+  if (length(parms$d) != last_t) parms$d <- rep(parms$d, times=(last_t))
+  # Now we "loop" and calculate N for each time
+  # notice that the code is *exactly* like the equation
+  for (i in seq_along(parms$t[-last_t])){
+    N[i+1] <- popfun(N[i], parms)
+  }
+  return(data.frame(t=parms$t, b = parms$b, d = parms$d, N = N))
+}
+
+exppop <- function(N0, parms){
+  N1 <- N0 * (1 + parms$b - parms$d)
+
+  return(N1)
+}
