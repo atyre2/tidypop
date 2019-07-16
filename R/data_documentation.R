b0504864ae06e89eb3c9e1f00772e250ee58360e
Drew Tyre <atyre2@unl.edu>
2019-07-16 15:12:42 -0500

    add data and package documentation

---
+++ R/data_documentation.R
@@ -0,0 +1,22 @@
+#' Helmeted Honeyeater projection matrices
+#'
+#' 3 age structured projection matrices for Helmeted Honeyeaters in 1991-1994, and
+#' the associated population vectors.
+#'
+#' This bird is critically endangered, restricted to 3 small populations
+#' just east of Melbourne. Zoos Victoria contributes to conservation by captive
+#' rearing individuals to supplement wild populations, as well as maintaining a
+#' captive population "lifeboat" for the species.
+#'
+#' @format two lists
+#' \describe{
+#'   \item{hh_A}{ a list of 3 10x10 projection matrices.}
+#'   \item{hh_N}{ a list of 4 vectors of length 10 with counts by age.}
+#' }
+#'
+#' @source Unfortunately I have no idea where this data came from.
+#'
+"hh_A"
+
+#' @rdname hh_A
+"hh_N"
