fbdffa9b448fe9d695827bdac61568dcb531d4dd
Drew Tyre <atyre2@unl.edu>
2019-07-30 14:28:07 -0500

    2nd draft createLandscape + documentation

--- R/MetaLandWrappers.R
+++ R/MetaLandWrappers.R
@@ -1,5 +1,3 @@
-
-
 #' Constructor function for landscape class objects
 #'
 #' @param parms list of arguments
@@ -11,12 +9,13 @@
 #' # calling the function without arguments gives a default landscape with 5 patches
 #' rl <- createLandscape()
 #' plot(rl)
-createLandscape <- function(parms = NULL){
-  rlandargs <- names(formals(MetaLandSim::rland.graph))
-  defaults <- list(mapsize = 1000, dist_m = 30, areaM = 2, areaSD = 0.2, Npatch = 5, disp = 500, plotG = FALSE )
-  if(!is.null(parms)){
-    provided <- rlandargs %in% names(parms)
-    defaults[rlandargs[provided]] <- parms[rlandargs[provided]]
+createLandscape <- function(...){
+  rlandargs <- formalArgs(MetaLandSim::rland.graph)
+  calledargs <- names(list(...))
+  defaults <- list(mapsize = 1000, dist_m = sqrt((1e5*2.4)/pi), areaM = 2, areaSD = 0.2, Npatch = 5, disp = 500, plotG = FALSE )
+  if(!is.null(calledargs)){
+    provided <- rlandargs %in% calledargs
+    defaults[rlandargs[provided]] <- list(...)[rlandargs[provided]]
   }
   result <- do.call(MetaLandSim::rland.graph, defaults)
   result
